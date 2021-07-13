bd51bd1b6c53cc57c9c9f839f7d4984fa597faf7
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-07-13 19:35:43 -0300

    criando PrivateRoute function
    Func para redirecionar o usuário caso não esteja autenticado

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 
 import {Home} from './pages/Home'
 
+const isAuthenticated = true
+
+function PrivateRoute({ component: Component, ...rest }) {
+    return (
+        <Route
+            {...rest}
+            render={props =>
+                isAuthenticated ? (
+                    <Component {...props} />
+                ) : (
+                    <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+                )
+            }
+         />
+    )
+}
 
 export function Routes() {
     return (
