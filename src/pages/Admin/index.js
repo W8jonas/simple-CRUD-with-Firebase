236e9a3954a45e0bbb313c1050f4b92020ee7f56
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-07-14 03:42:23 -0300

    criando pagina de admin

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
+import { Product } from '../../Components/Product'
 
 export function Admin() {
+    const [products, setProducts] = useState([{id: Math.random()}])
+
+    function handleAddData(event) {
+        event.preventDefault()
+        setProducts((prev) => [...prev, {id: Math.random()}]);
+    }
+
     return (
-        <h1>Pagina Admin</h1>
+        <div>
+            <h1>Painel de admin</h1>
+            
+            <h4>Produtos atuais</h4>
+            
+            {products.map((product, index) => (
+                <Product 
+                    key={product.id} 
+                    {...product} 
+                />
+            ))}
+
+            <button
+                className="button" 
+                onClick={handleAddData}
+            >
+                Adicionar novo produto
+            </button>
+        </div>
     )
-} 
+}
