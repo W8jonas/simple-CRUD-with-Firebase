d82845312c6c1d1c8e27dd62dfbd5b7f8c11cd6d
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-07-14 00:14:24 -0300

    criando estrutura html da pagina

--- src/pages/Auth/index.js
+++ src/pages/Auth/index.js
@@ -1,7 +1,36 @@
 import React from 'react'
+import './styles.css' 
+
 
 export function Auth() {
+    function handleSubmit(event) {
+        event.preventDefault()
+    }
+
     return (
-        <h1>Pagina Auth</h1>
+        <div id="container">
+            <h1>Pagina Autenticação</h1>
+
+            <h4>Você precisa estar autenticado para continuar</h4>
+
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Digite seu nome"
+                />
+
+                <input
+                    type="text"
+                    placeholder="Digite seu email"
+                />
+
+                <input
+                    type="text"
+                    placeholder="Digite sua senha"
+                />
+
+            </form>
+
+        </div>
     )
 } 
