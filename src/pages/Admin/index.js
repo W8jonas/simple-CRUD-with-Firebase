194878f37da975cae2104f82554aa97cb2bded57
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-07-14 18:51:43 -0300

    Criando produtos no Firebase

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,12 +1,23 @@
 import React, { useCallback, useState } from 'react'
 import { Product } from '../../Components/Product'
+import { auth, database } from '../../services/firebase';
 
 export function Admin() {
-    const [products, setProducts] = useState([{id: Math.random()}])
+    const [products, setProducts] = useState([{id: Math.round(Math.random() * 100000).toString()}])
 
     function handleAddData(event) {
         event.preventDefault()
-        setProducts((prev) => [...prev, {id: Math.random()}]);
+        const user = auth.currentUser;
+        const { uid } = user
+
+        database.collection("userProducts").doc(uid).collection("products").add({})
+            .then((docRef) => {
+                console.log(docRef.id)
+                setProducts((prev) => [...prev, {id: docRef.id}]);
+            })
+            .catch((error) => {
+                console.error("Error adding document: ", error);
+            })
     }
 
     return (
@@ -17,8 +28,8 @@ export function Admin() {
             
             {products.map((product, index) => (
                 <Product 
-                    key={product.id} 
-                    {...product} 
+                    key={product.id}
+                    id={product.id}
                 />
             ))}
 
