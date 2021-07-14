37306e982c2cc5a4c1359d9e0dbe42e6cd18be47
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-07-14 18:51:54 -0300

    add Firestore

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app';
 
 import 'firebase/auth';
 import 'firebase/database'
+import 'firebase/firestore'
 
 var firebaseConfig = {
     apiKey: "AIzaSyCae4w9_fVvNFdimNhSI1bHZ0YmajraYg8",
@@ -16,6 +17,6 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
-const database = firebase.database(); 
+const database = firebase.firestore(); 
 
 export { firebase, auth, database } 
