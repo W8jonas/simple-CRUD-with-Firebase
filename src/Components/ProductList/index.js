3be0f61390d181a0008e9eafd03267c91f6b0b1d
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-07-15 01:18:55 -0300

    ajustando nome dos estilos

--- src/Components/ProductList/index.js
+++ src/Components/ProductList/index.js
@@ -4,13 +4,13 @@ import './styles.css'
 export function ProductList({id: productId, formData}) {
 
     return (
-        <div className="Container">
+        <div className="container-product-list">
             <h3>Id do produto: {productId}</h3>
             <div>
                 {formData.map((item, index) => {
                     return Object.entries(item)[0][0] !== "id" 
                     ? (
-                        <div key={`${index}`} className="rowContainer">
+                        <div key={`${index}`} className="row-container-product-list">
                             <p>{Object.entries(item)[0][0]}: </p>
                             <p>{Object.entries(item)[0][1]}</p>
                         </div>
