a954e2323b41cbd75a1416f41243010693ce5fad
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-07-14 22:55:46 -0300

    iniciando estrutura tela home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { ProductList } from '../../Components/ProductList'
+import { database } from '../../services/firebase';
 
 export function Home() {
+
     return (
-        <h1>Pagina Home</h1>
+        <div>
+            <h1>Pagina Home</h1>
+            
+            <h4>Produtos atuais</h4>
+            
+            {products.map((product) => (
+                <ProductList
+                    key={product.id}
+                    id={product.id}
+                />
+            ))}
+
+            <button
+                className="button" 
+                // onClick={handleAddData}
+            >
+                Para adicionar um novo produto cadastre-se
+            </button>
+        </div>
     )
 } 
