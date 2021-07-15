cf152b3062faebaf62e19717e2aabedfc0d7dbc7
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-07-15 01:09:15 -0300

    recebendo os posts do firebase

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,18 +3,67 @@ import { ProductList } from '../../Components/ProductList'
 import { database } from '../../services/firebase';
 
 export function Home() {
+    const [users, setUsers] = useState([])
+    const [products, setProducts] = useState([[],[]])
+
+    useEffect(() => {
+        database.collection("users")
+            .get()
+            .then((querySnapshot) => {
+                const arrayOfUsers = []
+                querySnapshot.forEach((doc) => {
+                    arrayOfUsers.push({...doc.data()})
+                })
+                setUsers(arrayOfUsers)
+            })
+    }, [])
+
+    useEffect(() => {
+        async function getProducts() {
+            const promises = users.map(async (user, index) => {
+                const response = await database.collection("userProducts")
+                    .doc(user.uid)
+                    .collection("products")
+                    .get()
+                
+                    const arraySnapshot = []
+
+                    response.forEach((doc) => {
+                        arraySnapshot.push({id: doc.id, ...doc.data()})
+                    })
+                    
+                    console.log('arraySnapshot:', arraySnapshot)
+                    productsSeparatedByUsers.push(arraySnapshot)
+            })
+
+            await Promise.all(promises)
+        }
+
+        if(!users.length) return 
+
+        const productsSeparatedByUsers = []
+        getProducts().then(() => setProducts(productsSeparatedByUsers))
+        console.log('productsSeparatedByUsers:', productsSeparatedByUsers)
+        
+    }, [users])
 
     return (
         <div>
             <h1>Pagina Home</h1>
             
-            <h4>Produtos atuais</h4>
-            
-            {products.map((product) => (
-                <ProductList
-                    key={product.id}
-                    id={product.id}
-                />
+            {users.map((user, index) => (
+                <div key={user.uid}>
+                    <h4>Produtos atuais do {user.name}</h4>
+                    
+                    {products[index] && products[index].map((product) => (
+                        <ProductList
+                            key={product.id}
+                            id={product.id}
+                            formData={Object.entries(product).map((subArray) => ({ [subArray[0]]:subArray[1] }))}
+
+                        />
+                    ))}
+                </div>
             ))}
 
             <button
