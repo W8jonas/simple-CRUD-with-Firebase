f62124e3ed62f96c801fe3710a4d345323378330
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-07-15 01:08:45 -0300

    ajustando ProductList

--- src/Components/ProductList/index.js
+++ src/Components/ProductList/index.js
@@ -2,34 +2,21 @@ import React from 'react'
 import './styles.css'
 
 export function ProductList({id: productId, formData}) {
+
     return (
         <div className="Container">
             <h3>Id do produto: {productId}</h3>
-            <div className="rowContainer">
-                <div>
-                    {formData.map((item, index) => {
-                        return Object.entries(item)[0][0] !== "id" 
-                        ? (
-                            <div key={`${index}`} className="rowContent">
-                                <input
-                                    type="text"
-                                    name="Attribute"
-                                    placeholder="Atributo"
-                                    value={Object.entries(item)[0][0]}
-                                />
-
-                                <input
-                                    type="text"
-                                    name="Value"
-                                    placeholder="Value"
-                                    value={Object.entries(item)[0][1]}
-                                />
-                            </div>
-                        )
-                        : null
-                    })}
-                </div>
-
+            <div>
+                {formData.map((item, index) => {
+                    return Object.entries(item)[0][0] !== "id" 
+                    ? (
+                        <div key={`${index}`} className="rowContainer">
+                            <p>{Object.entries(item)[0][0]}: </p>
+                            <p>{Object.entries(item)[0][1]}</p>
+                        </div>
+                    )
+                    : null
+                })}
             </div>
         </div>
     )
