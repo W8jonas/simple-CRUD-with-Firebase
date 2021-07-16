fd50030e8bf0e589afa1045d11e605cc00ff72e2
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-07-15 23:34:36 -0300

    atualizando Input para unform

--- src/Components/Input/index.js
+++ src/Components/Input/index.js
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
+import { useField } from '@unform/core'
 
 import './styles.css'
 
-export function Input({name, label, ...rest}) {
+export function Input({ name, label, ...rest }) {
+    const inputRef = useRef(null)
+    const { fieldName, defaultValue, registerField, error } = useField(name)
+
+    useEffect(() => {
+        registerField({
+            name: fieldName,
+            ref: inputRef,
+            getValue: ref => {
+                return ref.current.value
+            },
+            setValue: (ref, value) => {
+                ref.current.value = value
+            },
+            clearValue: ref => {
+                ref.current.value = ''
+            },
+        })
+    }, [fieldName, registerField])
+
     return (
         <div className="input-block">
-            <label htmlFor={name}>{label}</label>
-            <input id={name} {...rest}/>
+
+            <label htmlFor={fieldName}>{label}</label>
+
+            <input
+                id={fieldName}
+                ref={inputRef}
+                defaultValue={defaultValue}
+                {...rest}
+            />
+
+            {error && <span className="error">{error}</span>}
         </div>
     )
 }
+
